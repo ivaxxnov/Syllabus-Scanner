7ad227ad10641d381df909d87813a60c509eb0fa
kian <kian@pop-os.localdomain>
2023-12-10 02:24:39 -0500

    made first outline

---
+++ data_pipeline.js
@@ -0,0 +1,49 @@
+/*
+The code in this file is meant to be for pdf processing purposes only!
+Do not add any website aesthetic type code to this file or i will find you and *****.
+*/
+
+
+/*
+extract text from pdf
+-- parameters: pdf file, as an object
+-- return: string
+*/
+function stringFromPDF(file) {
+}
+
+
+/*
+build prompt to send to chatgdp
+-- parameters: pdf in string format
+-- return: prompt ready to send
+*/
+function buildPrompt(pdf) {
+}
+
+
+/*
+send prompt to chatgdp and return response
+-- parameters: prompt
+-- return: chatgdp response
+*/
+function queryGPT(prompt) {
+}
+
+
+/*
+check response to see if its dookie
+-- parameters: response
+-- return: boolena
+*/
+function checkResponse(resposne) {
+}
+
+
+/*
+build spreadsheet file with response
+-- parameters: response
+-- return: file (as object)
+*/
+function buildSpreadsheet(response) {
+}
