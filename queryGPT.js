f97e557d7d494a5103b5ee450d23180d7ba47866
kian <kian@pop-os.localdomain>
2023-12-18 23:55:02 -0500

    changed function call to gpt3.5

--- queryGPT.js
+++ queryGPT.js
@@ -3,7 +3,6 @@ Seperate Function for building query GPT function.
 */
 
 
-
 /*
 build prompt to send to chatgdp
 -- parameters: pdf in string format
@@ -24,14 +23,15 @@ function queryGPT(prompt) {
 	const endpoint = 'https://api.openai.com/v1/completions';
 
 	const data = {
-		model: 'babbage-002',
+		model: 'gpt-3.5-turbo',
 		prompt: prompt,
 		temperature: .5, // 0-2. 0 is deterministic. 2 is more creative
-		max_tokens: 2048, // max 2048
+		max_tokens: 4097, // max
 		top_p: 1, // 0-1. 0 is more diverse.
 		frequency_penalty: 0, // 0-2. 2 is less repetitive.
 		presence_penalty: 0 // 0-2.
 	};
+
 	console.log("start fetch")
 	fetch(endpoint, {
 		method: 'POST',
